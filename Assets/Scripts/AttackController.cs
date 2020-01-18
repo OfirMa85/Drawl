6e6b0eeed5cb0f4cefbbeaff9d3c557d51b7a993
OfirMa85 <OfirMa85@gmail.com>
2020-01-18 16:48:58 +0200

    Attacks now destroy shield orbs

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -6,6 +6,41 @@ public class AttackController : MonoBehaviour
 {
     public int playerId;
     public AudioClip clip;
+    public float[] damageTimes;
+
+    int damage;
+    private int state;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        damage = 3;
+        state = 0;
+        cam = Camera.main;
+
+        Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        transform.position = mousePosition;
+
+        StartCoroutine(LowerDamage());
+    }
+
+    private void Update()
+    {
+        AttackControl();
+    }
+
+    IEnumerator LowerDamage()
+    {
+        yield return new WaitForSeconds(damageTimes[state]);
+        state++;
+        damage--;
+        if (damage > 0)
+            StartCoroutine(LowerDamage());
+        else
+            Destroy(gameObject);
+    }
 
     public void DestroyAttack(bool playOOF)
     {
@@ -13,5 +48,52 @@ public class AttackController : MonoBehaviour
         if (playOOF)
             AudioSource.PlayClipAtPoint(clip, transform.position);
 
+    }
+
+    private void AttackControl()
+    {
+        //If an attack is on the scene - control it
+        if (Input.GetMouseButton(0))
+        {
+            //mouse position in world coords
+            Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+
+            int layerMask = LayerMask.GetMask("Hittable");
+
+            //raycast between each 2 points of the trail, to detect hittables that were skipped
+            RaycastHit2D[] hits = Physics2D.LinecastAll(transform.position, mousePosition, layerMask);
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider != null)
+                {
+                    //Something was hit
+                    if (hit.collider.tag == "Obstacle")
+                    {
+                        //Obstacle was hit - destroy attack
+                        DestroyAttack(true);
+                    }
+                    else if (hit.collider.tag == "Player")
+                    {
+                        //PLayer was hit
+                        if (hit.collider.GetComponent<PlayerController>().playerId != playerId)
+                        {
+                            Debug.Log(damage + " damage");
+                            hit.collider.GetComponent<PlayerController>().GotHit(damage);
+                            DestroyAttack(false);
+                        }
+                    }
+                }
+            }
+
+            transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
+        }
+        //Mouse button up, destroy attack
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //destroy attack, no sound
+            DestroyAttack(false);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,8 @@ public class PlayerController : MonoBehaviour
     //Initialize variables
 
     public int playerId; //1 or 2 - player's id
-    public float maxHealth;
     public GameObject attackPrefab; //attack prefab
     public GameObject shieldOrbPrefab; //shield orb prefab
-    public float damage;
     public int maxShieldOrbs;
     public float shieldOffset;
     public float shieldRotationSpeed;
@@ -23,22 +21,15 @@ public class PlayerController : MonoBehaviour
     private GameObject shield; //shield orbs parent
     private AudioSource audioSource;
 
-    private float health;
     private int shieldOrbs;
 
     void Start()
     {
         cam = Camera.main;
-        health = maxHealth;
         shieldOrbs = maxShieldOrbs;
         audioSource = GetComponent<AudioSource>();
 
         CreateShield();
-    }
-
-    void Update()
-    {
-        AttackControl();
     }
 
     private void CreateShield()
@@ -58,75 +49,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void AttackControl()
+    //Player got hit
+    public void GotHit(int damage)
     {
-        //If an attack is on the scene - control it
-        if (Input.GetMouseButton(0) && currentAttack != null)
-        {
-            //mouse position in world coords
-            Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0;
-
-            int layerMask = LayerMask.GetMask("Hittable");
-
-            //raycast between each 2 points of the trail, to detect hittables that were skipped
-            RaycastHit2D[] hits = Physics2D.LinecastAll(currentAttack.transform.position, mousePosition, layerMask);
-            bool destroyed = false;
-
-            foreach (RaycastHit2D hit in hits)
-            {
-                if (hit.collider != null)
-                {
-                    //Something was hit
-                    if (hit.collider.tag == "Obstacle")
-                    {
-                        //Obstacle was hit - destroy attack
-                        currentAttack.GetComponent<AttackController>().DestroyAttack(true);
-                        destroyed = true;
-                    }
-                    else if (hit.collider.tag == "Player")
-                    {
-                        if (hit.collider.gameObject != gameObject)
-                        {
-                            hit.collider.GetComponent<PlayerController>().GotHit(damage);
-                            currentAttack.GetComponent<AttackController>().DestroyAttack(false);
-                            destroyed = true;
-                        }
-                    }
-                }
-            }
-
-            //move attack to mouse pos
-            if (!destroyed)
-                currentAttack.transform.position = new Vector3(mousePosition.x, mousePosition.y, 0);
-        }
-        //Mouse button up, destroy attack
-        else if (Input.GetMouseButtonUp(0) && currentAttack != null)
-        {
-            //destroy attack, no sound
-            currentAttack.GetComponent<AttackController>().DestroyAttack(false);
-        }
+        audioSource.PlayOneShot(audioClips[0]);
+        DamageShieldOrbs(damage);
     }
 
-    //Player got hit
-    public void GotHit(float damage)
+    public void DamageShieldOrbs(int damage)
     {
-        health -= damage;
-        audioSource.PlayOneShot(audioClips[0]);
+        int leftoverDamage = 0;
+        if (shield.transform.childCount > 0)
+            leftoverDamage = shield.transform.GetChild(0).GetComponent<ShieldOrbController>().GotHit(damage);
+        if (shield.transform.childCount > 0 && leftoverDamage > 0)
+            shield.transform.GetChild(0).GetComponent<ShieldOrbController>().GotHit(leftoverDamage);
     }
 
     //Player clicked, start an attack
     private void OnMouseDown()
     {
-        //If an attack doenst exist already - if mousebuttonup didnt register
-        if (currentAttack != null)
-            DestroyImmediate(currentAttack);
 
-        //Initialize an attack
-        currentAttack = Instantiate(attackPrefab, transform.position, transform.rotation);
+        //Initialize an attacks
+        currentAttack = Instantiate(attackPrefab, transform.parent);
         currentAttack.GetComponent<AttackController>().playerId = playerId;
-        Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0;
-        attackPrefab.transform.position = mousePosition;
     }
 }
