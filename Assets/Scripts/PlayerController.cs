3eb38919c9651a91de408ee4a1f211f64ae3a7ba
OfirMa85 <OfirMa85@gmail.com>
2020-01-18 16:52:59 +0200

    Shield removed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,66 +9,32 @@ public class PlayerController : MonoBehaviour
 
     public int playerId; //1 or 2 - player's id
     public GameObject attackPrefab; //attack prefab
-    public GameObject shieldOrbPrefab; //shield orb prefab
-    public int maxShieldOrbs;
-    public float shieldOffset;
-    public float shieldRotationSpeed;
+    public float maxHealth;
 
     public AudioClip[] audioClips; //array of audioclips saved on the player
 
     private Camera cam; //main cam
     private GameObject currentAttack; //current attack that is summoned
-    private GameObject shield; //shield orbs parent
     private AudioSource audioSource;
 
-    private int shieldOrbs;
+    private float health;
 
     void Start()
     {
         cam = Camera.main;
-        shieldOrbs = maxShieldOrbs;
+        health = maxHealth;
         audioSource = GetComponent<AudioSource>();
-
-        CreateShield();
-    }
-
-    private void CreateShield()
-    {
-        shield = new GameObject("Shield");
-        shield.transform.parent = transform.parent;
-        shield.transform.localPosition = Vector2.zero;
-
-        for (int i = 0; i < maxShieldOrbs; i++)
-        {
-            GameObject shieldOrbGO = Instantiate(shieldOrbPrefab, shield.transform);
-            shieldOrbGO.transform.position = transform.position + new Vector3(Mathf.Sin(Mathf.Deg2Rad * (float)i * (360 / maxShieldOrbs)) * shieldOffset, Mathf.Cos(Mathf.Deg2Rad * (float)i * (360 / maxShieldOrbs)) * shieldOffset);
-            ShieldOrbController shieldOrbController = shieldOrbGO.GetComponent<ShieldOrbController>();
-            shieldOrbController.shieldOffset = shieldOffset;
-            shieldOrbController.rotationSpeed = shieldRotationSpeed;
-            shieldOrbController.rotation = ((float)i * (360 / maxShieldOrbs));
-        }
     }
 
     //Player got hit
     public void GotHit(int damage)
     {
         audioSource.PlayOneShot(audioClips[0]);
-        DamageShieldOrbs(damage);
-    }
-
-    public void DamageShieldOrbs(int damage)
-    {
-        int leftoverDamage = 0;
-        if (shield.transform.childCount > 0)
-            leftoverDamage = shield.transform.GetChild(0).GetComponent<ShieldOrbController>().GotHit(damage);
-        if (shield.transform.childCount > 0 && leftoverDamage > 0)
-            shield.transform.GetChild(0).GetComponent<ShieldOrbController>().GotHit(leftoverDamage);
     }
 
     //Player clicked, start an attack
     private void OnMouseDown()
     {
-
         //Initialize an attacks
         currentAttack = Instantiate(attackPrefab, transform.parent);
         currentAttack.GetComponent<AttackController>().playerId = playerId;
